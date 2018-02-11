a6f2b884a3d6ae6961f09a384d82f397d691e9d4
felk <de.felix.koenig@gmail.com>
2018-02-11 18:15:04 +0100

    add smart autoscroll to chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import "./Chat.css"
 import ChatMessage from "./ChatMessage";
@@ -9,18 +10,27 @@ class Chat extends Component {
     constructor() {
         super();
         this.state = {
-            chatmessages: []
+            chatmessages: [],
+            anchored: true
         };
-        // this.handleChange = this.handleChange.bind(this);
+        this.scroll_anchor = null;
+        this.scrollarea_dom_element = null;
+        this.last_scroll_offset = 0;
         this.chatEvent = this.chatEvent.bind(this);
+        this.onScroll = this.onScroll.bind(this);
+        this.onReanchor = this.onReanchor.bind(this);
+        this.onResize = this.onResize.bind(this);
     }
 
     componentDidMount() {
         this.context.ocs.chatService.listenOnMessage(this.chatEvent);
+        window.addEventListener("resize", this.onResize);
+        this.scrollarea_dom_element = ReactDOM.findDOMNode(this).querySelector(".scrollArea");
     }
 
     componentWillUnmount() {
         this.context.ocs.chatService.unlistenOnMessage(this.chatEvent);
+        window.removeEventListener("resize", this.onResize);
     }
 
     chatEvent(data) {
@@ -29,12 +39,61 @@ class Chat extends Component {
         this.setState({chatmessages: newChatmessages});
     }
 
+    scrollToBottom() {
+        if (this.state.anchored) {
+            this.scroll_anchor.scrollIntoView({behavior: "smooth"});
+        }
+    }
+
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
+    onScroll(event) {
+        const scroll_offset = this.scrollarea_dom_element.scrollTop;
+        const scroll_delta = scroll_offset - this.last_scroll_offset;
+        this.last_scroll_offset = scroll_offset;
+
+        const max_scroll_offset = this.scrollarea_dom_element.scrollHeight - this.scrollarea_dom_element.offsetHeight;
+         if (scroll_delta < 0 && event.type !== "resize") {
+            // user scrolled up, un-anchor!
+            this.setState({anchored: false});
+        } else if (scroll_offset >= max_scroll_offset) {
+             // scrolled all the way to the bottom, re-anchor
+            this.setState({anchored: true});
+        }
+    }
+
+    /**
+     * We need to re-adjust the scrolling stuff if the window changes in size.
+     * This also includes opening or closing a keyboard on mobile devices for example.
+     */
+    onResize(event) {
+        this.onScroll(event);
+        this.scrollToBottom();
+    }
+
+    onReanchor() {
+        this.setState({anchored: true});
+    }
+
     render() {
         const chatmessages = this.state.chatmessages
             .map(message => <ChatMessage message={message} key={uuid.v4()}/>);
+        const scrollToBottomBar = this.state.anchored
+            ? null
+            : <button className="scrollToBottomBar" onClick={this.onReanchor}>
+                Click to re-enable autoscroll to bottom...</button>;
+        const scrollArea = (
+            <div className="scrollArea" onScroll={this.onScroll}>
+                {chatmessages}
+                <div ref={elem => {this.scroll_anchor = elem;}}>&nbsp;</div>
+            </div>
+        );
         return (
             <div className="Chat">
-                {chatmessages}
+                {scrollArea}
+                {scrollToBottomBar}
             </div>
         );
     }
